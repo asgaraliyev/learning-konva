6eb7772b938356e70250c653f6f369fa021917e2
Asgar Aliyev <47985813+asgaraliyev@users.noreply.github.com>
2021-06-06 22:48:24 +0400

    as

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import "./styles.scss";
 import { Stage, Layer, Circle } from "react-konva";
 import Image from "./components/Image";
 export default function App() {
+  function handleFile(e) {}
   return (
     <>
-      <button>Add Image</button>
+      <input type="file" onChange={handleFile}></input>
       <Stage
         width={window.innerWidth}
         height={window.innerHeight}
         style={{ backgroundImage: 'url( "/background.png")' }}
       >
         <Layer>
-          <Image url="https://konvajs.org/assets/yoda.jpg" />
+          <Image url="https://konvajs.org/assets/yoda.jpg" draggable />
+          <Image url="https://konvajs.org/assets/yoda.jpg" draggable />
         </Layer>
       </Stage>
     </>
